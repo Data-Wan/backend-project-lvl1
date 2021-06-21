6f783086b444ef432de9c31953ad53e73ebb8e7c
Data-Wan <ustimenko.work@gmail.com>
2021-06-21 14:54:01 +0300

    add prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,37 @@
+import {getAnswer, getRandomInt, startSpecificGame} from '../index.js';
+
+function isPrime(num) {
+    let notPrimeAnswer = 'no';
+    let primeAnswer = 'yes';
+
+    for (let i = 2; i < num / 2; i += 1)
+        if (num % i === 0) {
+            return notPrimeAnswer;
+        }
+    if (num > 1) {
+        return primeAnswer;
+    }
+    return notPrimeAnswer;
+}
+
+function gamePredicateFunction() {
+
+
+    const number = getRandomInt(100);
+
+    const rightAnswer = isPrime(number);
+
+
+    const userAnswer = getAnswer(number);
+
+    return [
+        rightAnswer === userAnswer,
+        rightAnswer,
+        userAnswer,
+    ];
+}
+
+const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".\n';
+const numberOfRounds = 3;
+
+startSpecificGame(gameRules, gamePredicateFunction, numberOfRounds);
