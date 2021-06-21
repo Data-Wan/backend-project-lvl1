659e2ea4d34223df608261fe6051762931e3fb9e
Data-Wan <ustimenko.work@gmail.com>
2021-06-21 11:58:15 +0300

    fix linter errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,14 +2,10 @@
 
 import readlineSync from 'readline-sync';
 
-
-const startGameMessage = "Welcome to the Brain Games!\n"
-const askNameMessage =  "May I have your name? "
-const greetingMessage = "Hello, %s!";
+const startGameMessage = 'Welcome to the Brain Games!\n';
+const askNameMessage = 'May I have your name? ';
+const greetingMessage = 'Hello, %s!';
 
 console.log(startGameMessage);
 const name = readlineSync.question(askNameMessage);
 console.log(greetingMessage, name);
-
-
-
